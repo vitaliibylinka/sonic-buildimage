af41fdb42ea008cd4c7bcf5fdaf6617e75c2ae20
Vitalii Bylinka <vitalii.bylinka@plvision.eu>
2023-02-16 12:22:44 +0200

    Fix docker files

--- rules/stp.mk
+++ rules/stp.mk
@@ -1,9 +1,8 @@
 # STP package
 #
 STP_VERSION = 1.0.0
-export STP_VERSION
 
-STP = stp_$(STP_VERSION)_amd64.deb
+STP = stp_$(STP_VERSION)_$(CONFIGURED_ARCH).deb
 $(STP)_SRC_PATH = $(SRC_PATH)/sonic-stp
 $(STP)_DEPENDS += $(LIBEVENT)
 $(STP)_DEPENDS += $(LIBSWSSCOMMON_DEV)
@@ -11,8 +10,4 @@ $(STP)_RDEPENDS += $(LIBSWSSCOMMON)
 SONIC_DPKG_DEBS += $(STP)
 
 STP_DBG = stp-dbg_1.0.0_amd64.deb
-$(STP_DBG)_DEPENDS += $(STP)
-$(STP_DBG)_RDEPENDS += $(STP)
 $(eval $(call add_derived_package,$(STP),$(STP_DBG)))
-
-export STP
